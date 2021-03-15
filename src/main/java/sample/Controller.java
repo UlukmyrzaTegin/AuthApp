429111f058ef8062ac3c964e011a04b59e3992cf
UlukmyrzaTegin <ulukmyrza.zhakypbekov@gmail.com>
2021-03-15 17:11:53 +0600

    Создан к каждому элементы управления id и метод onBtnClicked

--- src/main/java/sample/Controller.java
+++ src/main/java/sample/Controller.java
@@ -1,4 +1,31 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+
 public class Controller {
+
+    @FXML
+    private TextField txtLogin;
+
+    @FXML
+    private PasswordField txtPassword;
+
+    @FXML
+    private Button btnEnter;
+
+    @FXML
+    private Button btnCancel;
+
+    @FXML
+    void onBtnClicked(ActionEvent event) {
+
+    }
+
+
+
 }
