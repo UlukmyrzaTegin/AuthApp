773740d7a41f501c45feec69a8ea310822cb4739
UlukmyrzaTegin <ulukmyrza.zhakypbekov@gmail.com>
2021-03-23 15:59:02 +0600

    Для класса Department добавлен throw,RuntimeException (unchecked exceptions), служит для проверяемых исключений (проверка длинна name).

--- src/main/java/sample/models/Department.java
+++ src/main/java/sample/models/Department.java
@@ -5,4 +5,29 @@ package sample.models;
  * 3/16/2021
  */
 public class Department {
+    private int id;
+    private String name;
+
+    public Department(int id, String name) {
+        setId(id);
+        setName(name);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        if (name.isEmpty() || name.length() < 5)
+            throw new RuntimeException("Неверное значение для названия!");
+        this.name = name;
+    }
 }
